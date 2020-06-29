46527aa3b6a442c3763eaca5b07589d8ba620ae6
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-06-29 02:16:53 -0300

    Teste2

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello World!!</Text>
       <StatusBar style="auto" />
     </View>
   );
