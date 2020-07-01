cc9dd88d4c276befd8792dad23eae7d51a7426a8
Pedro Augusto <p.augusto0210@gmail.com>
2020-07-01 00:39:15 -0300

    FINISHED HOME PAGE

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import App from './src/index'
 
 export default function Main() {
+
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="dark-content"
+        style="auto"
+        backgroundColor="transparent"
+        translucent
+       />
       <App />
     </>
   );
--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -5,30 +5,34 @@ import { StyleSheet } from 'react-native'
       flex:1,
       backgroundColor:'#fdf7fa',
     },
+
     header: {
       flex:1,
       alignItems: 'center',
     },
+
     footer: {
       flex: 0.7,
       padding:25,
       backgroundColor: '#f3ca40',
     },
+
     imageBackground:{
-      flex: 2,
-      width: '100%',
-      resizeMode: 'contain',
-      top: 120,
+      flex: 1,
+      width: '90%',
+      top: '20%',
+      left: '7%',
       backgroundColor: '#fdf7fa',
       marginBottom: -160,
-      marginLeft: 10,
     },
+
     imageWave:{
       alignItems: 'center',
       justifyContent: 'center',
       left: 0,
-      top: 300,
+      top: '60%',
     },
+
     action: {
       flexDirection:'row',
       borderBottomWidth:1,
@@ -36,15 +40,18 @@ import { StyleSheet } from 'react-native'
       width: '90%',
       alignItems: 'center',
     },
+
     textInput: {
       marginTop:30,
       paddingBottom:10,
       color:'#fff'
     },
+
     button_container: {
       alignItems: 'center',
       justifyContent:'center'
     },
+
     button: {
       backgroundColor: '#13528B',
       height: 60,
@@ -54,6 +61,7 @@ import { StyleSheet } from 'react-native'
       alignItems: 'center',
       marginTop: 40,
     },
+
     buttonIcon: {
       height: 60,
       width: 60,
@@ -61,6 +69,7 @@ import { StyleSheet } from 'react-native'
       justifyContent: 'center',
       alignItems: 'center'
     },
+
     buttonText: {
       flex: 1,
       justifyContent: 'center',
@@ -69,6 +78,7 @@ import { StyleSheet } from 'react-native'
       color: '#FFF',
       fontSize: 16,
     },
+
     signUp: {
       flexDirection:'row',
       justifyContent:'center',
