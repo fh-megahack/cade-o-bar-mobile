26875d2e54c29a073315c119fbeba22dd7d467f3
André Kaoru Fuziyama <andrefuzi@MacBook-Pro-de-Andre.local>
2020-07-01 09:42:04 -0300

    fix errors | statusBar | Home template

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MapView, {Marker} from 'react-native-maps'
-import { SafeAreaView, View, Image, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
+import { StatusBar, View, Image, Text } from 'react-native';
 
 // Estilos
 import styles from './styles'
@@ -8,81 +8,82 @@ import BottomBar from '../../components/Navigator';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <>
+      <StatusBar barStyle="light-content" />
       <View style={styles.container}>
 
         <View style={styles.header}>
-          <Image style={styles.profileImage} source={require('../../assets/home/profilePedro.png') }/>
+          <Image style={styles.profileImage} source={require('../../assets/home/profilePedro.png')} />
           <View>
-            <Text style={styles.welcomeText}>Olá <Text style={styles.welcomeTextName}>Pedro</Text>,</Text> 
+            <Text style={styles.welcomeText}>Olá <Text style={styles.welcomeTextName}>Pedro</Text>,</Text>
             <Text style={styles.welcomeText}>Que tal descobrir novas experiencias hoje? </Text>
           </View>
         </View>
 
 
         <View style={styles.mapContainer}>
-                <MapView
-                  style={styles.map}
-                  initialRegion={{
-                    latitude: -23.620960, 
-                    longitude: -46.698819,
-                    latitudeDelta: 0.014,
-                    longitudeDelta: 0.014,
-              }}
-              >
-                  <Marker
-                    style={styles.mapMarker}
-                    coordinate={{
-                      latitude: -23.620960, 
-                      longitude: -46.698819,
-                    }}>
-                      <View style={styles.mapMarkerContainer}>
-                        <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoZe.png') }/>
-                        <Text style={styles.mapMarkerTitle}>Bar do Zé</Text>
-                      </View>
-                  </Marker>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -23.620960,
+              longitude: -46.698819,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            <Marker
+              style={styles.mapMarker}
+              coordinate={{
+                latitude: -23.620960,
+                longitude: -46.698819,
+              }}>
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoZe.png')} />
+                <Text style={styles.mapMarkerTitle}>Bar do Zé</Text>
+              </View>
+            </Marker>
 
-                  <Marker
-                    style={styles.mapMarker}
-                    coordinate={{
-                      latitude: -23.626945,
-                      longitude: -46.692290,
-                    }}>
-                      <View style={styles.mapMarkerContainer}>
-                        <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoPedro.png') }/>
-                        <Text style={styles.mapMarkerTitle}>Bar do Pedro</Text>
-                      </View>
-                  </Marker>
+            <Marker
+              style={styles.mapMarker}
+              coordinate={{
+                latitude: -23.626945,
+                longitude: -46.692290,
+              }}>
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoPedro.png')} />
+                <Text style={styles.mapMarkerTitle}>Bar do Pedro</Text>
+              </View>
+            </Marker>
 
-                  <Marker
-                    style={styles.mapMarker}
-                    coordinate={{
-                      latitude: -23.625401, 
-                      longitude: -46.702139,
-                    }}>
-                      <View style={styles.mapMarkerContainer}>
-                        <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoFuzi.png') }/>
-                        <Text style={styles.mapMarkerTitle}>Bar do Fuzi</Text>
-                      </View>
-                  </Marker>
+            <Marker
+              style={styles.mapMarker}
+              coordinate={{
+                latitude: -23.625401,
+                longitude: -46.702139,
+              }}>
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoFuzi.png')} />
+                <Text style={styles.mapMarkerTitle}>Bar do Fuzi</Text>
+              </View>
+            </Marker>
 
-                  <Marker
-                    style={styles.mapMarker}
-                    coordinate={{
-                      latitude: -23.614234, 
-                      longitude: -46.691110,
-                    }}>
-                      <View style={styles.mapMarkerContainer}>
-                        <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoMatheus.png') }/>
-                        <Text style={styles.mapMarkerTitle}>Bar do Matheus</Text>
-                      </View>
-                  </Marker>
-              </MapView>
+            <Marker
+              style={styles.mapMarker}
+              coordinate={{
+                latitude: -23.614234,
+                longitude: -46.691110,
+              }}>
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={require('../../assets/home/barDoMatheus.png')} />
+                <Text style={styles.mapMarkerTitle}>Bar do Matheus</Text>
+              </View>
+            </Marker>
+          </MapView>
         </View>
 
         <BottomBar />
 
       </View>
-    </SafeAreaView>
+    </>
   );
 }
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,79 +1,79 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#37323E',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#37323E',
+  },
 
-    header: {
-        flexDirection: 'row',
-        paddingHorizontal: 32,
-        paddingTop: '7%',
-        width: '90%',
-        marginBottom: 25,
-        alignItems: 'center'
-    },
+  header: {
+    flexDirection: 'row',
+    paddingHorizontal: 32,
+    paddingTop: 36,
+    width: '90%',
+    marginBottom: 16,
+    alignItems: 'center'
+  },
 
-    profileImage: {
-        width: 42,
-        height: 42,
-        borderRadius: 100,
-    },
+  profileImage: {
+    width: 42,
+    height: 42,
+    borderRadius: 100,
+  },
 
-    welcomeText: {
-        color: '#fff',
-        paddingHorizontal: 10,
-        fontSize: 13,
-    },
+  welcomeText: {
+    color: '#fff',
+    paddingHorizontal: 10,
+    fontSize: 13,
+  },
 
-    welcomeTextName: {
-        color: '#fff',
-        fontWeight: 'bold',
-    },
+  welcomeTextName: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 
-    mapContainer: {
-        flex: 1,
-        width: '100%',
-        borderTopRightRadius: 20,
-        borderTopStartRadius: 20,
-        overflow: 'hidden',
-      },
-    
-      map: {
-        height: '100%',
-        width: '100%',
-      },
-    
-      mapMarker: {
-        width: 90,
-        height: 65,
-      },
-    
-      mapMarkerContainer: {
-        width: 90,
-        height: 55,
-        backgroundColor: '#37323E',
-        flexDirection: 'column',
-        borderRadius: 8,
-        overflow: 'hidden',
-        alignItems: 'center'
-      },
-    
-      mapMarkerImage: {
-        width: 90,
-        height: 26,
-        resizeMode: 'cover',
-      },
-    
-      mapMarkerTitle: {
-        flex: 1,
-        color: '#fff',
-        fontSize: 8,
-        lineHeight: 23,
-      },
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderTopRightRadius: 20,
+    borderTopStartRadius: 20,
+    overflow: 'hidden',
+  },
 
-      footer: {
-          flex: 2
-      }
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 65,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 55,
+    backgroundColor: '#37323E',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 26,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    color: '#fff',
+    fontSize: 8,
+    lineHeight: 23,
+  },
+
+  footer: {
+    flex: 2
+  }
 });
